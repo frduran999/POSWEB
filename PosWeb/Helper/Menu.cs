2f317ffe6fdb160dcd84b51776700db1bfa2f2d8
Francisco <frduran.gonzalez@gmail.com>
2020-01-06 19:50:50 -0300

    vista venta

--- PosWeb/Helper/Menu.cs
+++ PosWeb/Helper/Menu.cs
@@ -16,47 +16,49 @@ namespace PosWeb.Helper
             var menu = new Control();
             var menulit = string.Empty;
             var datosUsuario = (ObjetoUsuario)HttpContext.Current.Session["VariableSesionUsuario"];
-
-            if (menu.MenuUsuario(datosUsuario.Perfil).Count > 0)
+            if (datosUsuario != null)
             {
-                var urlMenu = menu.MenuUsuario(datosUsuario.Perfil).ToList();
-                menulit = menulit + "<ul class='nav'>" +
-                    "<li class='nav-header'>Menu</li>";
-
-                for (var i = 0; i <= urlMenu.Count; i++)
+                if (menu.MenuUsuario(datosUsuario.Perfil).Count > 0)
                 {
-                    if (i > urlMenu.Count)
-                    {
-                        break;
-                    }
-                    menulit = menulit + string.Format("<li class='has-sub'>" +
-                    "<a href='javascript:;'>" +
-                    "<b class='caret'></b>");
-                    menulit = menulit + string.Format("<i class='{0}'></i><span>{1}</span></a>", urlMenu[i].Clase, urlMenu[i].PieMenu);
-                    menulit = menulit + string.Format("<ul class='sub-menu'>");
+                    var urlMenu = menu.MenuUsuario(datosUsuario.Perfil).ToList();
+                    menulit = menulit + "<ul class='nav'>" +
+                        "<li class='nav-header'>Menu</li>";
 
-                    var nombreModulo = urlMenu[i].PieMenu;
-                    for (var x = 0; x <= i; x++)
+                    for (var i = 0; i <= urlMenu.Count; i++)
                     {
-                        if (i >= urlMenu.Count)
+                        if (i > urlMenu.Count)
                         {
                             break;
                         }
-                        if (nombreModulo == urlMenu[i].PieMenu)
+                        menulit = menulit + string.Format("<li class='has-sub'>" +
+                        "<a href='javascript:;'>" +
+                        "<b class='caret'></b>");
+                        menulit = menulit + string.Format("<i class='{0}'></i><span>{1}</span></a>", urlMenu[i].Clase, urlMenu[i].PieMenu);
+                        menulit = menulit + string.Format("<ul class='sub-menu'>");
+
+                        var nombreModulo = urlMenu[i].PieMenu;
+                        for (var x = 0; x <= i; x++)
                         {
-                            menulit = menulit + string.Format("<li><a href='../{2}/{1}'>{0}</a></li>", urlMenu[i].Titulo,
-                                urlMenu[i].Action, urlMenu[i].Controller);
-                            i++;
+                            if (i >= urlMenu.Count)
+                            {
+                                break;
+                            }
+                            if (nombreModulo == urlMenu[i].PieMenu)
+                            {
+                                menulit = menulit + string.Format("<li><a href='../{2}/{1}'>{0}</a></li>", urlMenu[i].Titulo,
+                                    urlMenu[i].Action, urlMenu[i].Controller);
+                                i++;
+                            }
+                            else
+                            {
+                                i--;
+                                break;
+                            }
                         }
-                        else
-                        {
-                            i--;
-                            break;
-                        }
+                        menulit = menulit + "</ul>";
                     }
-                    menulit = menulit + "</ul>";
+                    menulit = menulit + "</li></ul>";
                 }
-                menulit = menulit + "</li></ul>";
             }
             return new MvcHtmlString(menulit);
         }
